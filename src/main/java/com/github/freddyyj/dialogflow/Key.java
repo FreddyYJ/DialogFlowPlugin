e102104754a6aabe0391e5e15fe8ffa00a4fdca9
김영재 <kyj141100@naver.com>
2020-02-09 12:17:11 +0900

    Add test key.json

---
+++ src/main/java/com/github/freddyyj/dialogflow/Key.java
@@ -0,0 +1,11 @@
+package com.github.freddyyj.dialogflow;
+
+public class Key {
+	private String projectID;
+	private String privateKeyID;
+	private String privateKey;
+	private static final String KEY_FILE="DialogFlowPlugin/key.json";
+	public Key() {
+		
+	}
+}
